58c05533f3628c50044ed95afb7354ad8b7eabd2
ErickSkrauch <erickskrauch@yandex.ru>
2016-07-29 12:14:32 +0300

    Revert "Revert "Реализована страница 404 ошибки""
    This reverts commit 22f80576bd903bd63c38bb29570e410ce8fb5b7c.

--- minecraft-skinsystem.go
+++ minecraft-skinsystem.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"runtime"
+	"time"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -12,16 +13,17 @@ import (
 	"elyby/minecraft-skinsystem/lib/services"
 )
 
+const redisString string = "redis:6379"
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	client, redisErr := redis.Dial("tcp", "redis:6379")
+	client, redisErr := redis.Dial("tcp", redisString)
 	if redisErr != nil {
 		log.Fatal("Redis unavailable")
 	}
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", routes.NotFound)
 	router.HandleFunc("/skins/{username}", routes.Skin).Methods("GET").Name("skins")
 	router.HandleFunc("/cloaks/{username}", routes.Cape).Methods("GET").Name("cloaks")
 	router.HandleFunc("/textures/{username}", routes.Textures).Methods("GET").Name("textures")
@@ -29,6 +31,8 @@ func main() {
 	router.HandleFunc("/minecraft.php", routes.MinecraftPHP).Methods("GET")
 	router.HandleFunc("/skins/", routes.SkinGET).Methods("GET")
 	router.HandleFunc("/cloaks/", routes.CapeGET).Methods("GET")
+	// 404
+	router.NotFoundHandler = http.HandlerFunc(routes.NotFound)
 
 	apiRouter := router.PathPrefix("/api").Subrouter()
 	apiRouter.HandleFunc("/user/{username}/skin", routes.SetSkin).Methods("POST")
@@ -36,5 +40,24 @@ func main() {
 	services.Redis = client
 	services.Router = router
 
+	go func() {
+		for {
+			time.Sleep(5 * time.Second)
+
+			resp := services.Redis.Cmd("PING")
+			if (resp.Err != nil) {
+				log.Println("Redis not pinged. Try to reconnect")
+				newClient, redisErr := redis.Dial("tcp", redisString)
+				if (redisErr != nil) {
+					log.Println("Cannot reconnect to redis")
+				} else {
+					services.Redis = newClient
+					log.Println("Reconnected")
+				}
+			}
+		}
+	}()
+
+	log.Println("Started");
 	log.Fatal(http.ListenAndServe(":80", router))
 }
