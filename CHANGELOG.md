dbefac0e849faa1fa2644963b4beb5654e7309fe
ErickSkrauch <erickskrauch@yandex.ru>
2020-04-23 21:10:42 +0300

    Resolves #21. Print hostname in the version command output

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased] - xxxx-xx-xx
+### Added
+- [#20](https://github.com/elyby/chrly/issues/20): Print hostname in the `version` command output.
+
 ### Fixed
 - [#22](https://github.com/elyby/chrly/issues/22): Correct version passing during building of the Docker image.
 
--- cmd/version.go
+++ cmd/version.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 
 	"github.com/spf13/cobra"
@@ -13,10 +14,16 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show the Chrly version information",
 	Run: func(cmd *cobra.Command, args []string) {
+		hostname, err := os.Hostname()
+		if err != nil {
+			hostname = "<unknown>"
+		}
+
 		fmt.Printf("Version:    %s\n", version.Version())
 		fmt.Printf("Commit:     %s\n", version.Commit())
 		fmt.Printf("Go version: %s\n", runtime.Version())
 		fmt.Printf("OS/Arch:    %s/%s\n", runtime.GOOS, runtime.GOARCH)
+		fmt.Printf("Hostname:   %s\n", hostname)
 	},
 }
 
